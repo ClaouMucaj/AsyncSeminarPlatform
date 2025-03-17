5a3e5140561a1ff4e272e5044e488be49cb85888
Klavdian <150528504+Klavdian@users.noreply.github.com>
2025-03-18 00:05:08 +0200

    test

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
-start cmd /k "cd seminar-system-backend && npm install && npm start"
-start cmd /k "cd seminar-system-frontend && npm install && npm start"
+start cmd /k "cd seminar-system-backend &&  npm install && npm start"
+start cmd /k "cd seminar-system-frontend &&  npm install && npm start"
